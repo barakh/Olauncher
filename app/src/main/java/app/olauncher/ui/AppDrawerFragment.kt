d6c286f93c0a8a4504ff6f1edf34bfddb166de9b
Barak <barakh@users.noreply.github.com>
2026-02-17 15:31:56 +0200

    Cleanup

--- app/src/main/java/app/olauncher/ui/AppDrawerFragment.kt
+++ app/src/main/java/app/olauncher/ui/AppDrawerFragment.kt
@@ -68,7 +68,7 @@ class AppDrawerFragment : Fragment() {
     private fun initViews() {
         if (flag == Constants.FLAG_HIDDEN_APPS)
             binding.search.queryHint = getString(R.string.hidden_apps)
-        else if (flag in Constants.FLAG_SET_HOME_APP_1..Constants.FLAG_SET_CALENDAR_APP)
+        else if (flag in Constants.HOME_APP_FLAGS || flag in Constants.FLAG_SET_SWIPE_LEFT_APP..Constants.FLAG_SET_CALENDAR_APP)
             binding.search.queryHint = "Please select an app"
         try {
             val searchTextView = binding.search.findViewById<TextView>(R.id.search_src_text)
@@ -223,15 +223,9 @@ class AppDrawerFragment : Fragment() {
                 return@setOnClickListener
             }
 
-            when (flag) {
-                Constants.FLAG_SET_HOME_APP_1 -> prefs.appName1 = name
-                Constants.FLAG_SET_HOME_APP_2 -> prefs.appName2 = name
-                Constants.FLAG_SET_HOME_APP_3 -> prefs.appName3 = name
-                Constants.FLAG_SET_HOME_APP_4 -> prefs.appName4 = name
-                Constants.FLAG_SET_HOME_APP_5 -> prefs.appName5 = name
-                Constants.FLAG_SET_HOME_APP_6 -> prefs.appName6 = name
-                Constants.FLAG_SET_HOME_APP_7 -> prefs.appName7 = name
-                Constants.FLAG_SET_HOME_APP_8 -> prefs.appName8 = name
+            if (flag in Constants.HOME_APP_FLAGS) {
+                val location = flag - Constants.FLAG_SET_HOME_APP_1 + 1
+                prefs.setAppName(location, name)
             }
             findNavController().popBackStack()
         }
